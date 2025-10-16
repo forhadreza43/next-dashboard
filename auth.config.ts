a470a3babeabaea00bed48babde93c6fc333fe18
Forhad Reza <forhad.bimt@gmail.com>
2025-10-16 23:40:56 +0600

    update AUTH_URL

--- auth.config.ts
+++ auth.config.ts
@@ -1,5 +1,4 @@
 import type { NextAuthConfig } from "next-auth";
-import { NextResponse } from "next/server";
 
 export const authConfig = {
   pages: {
@@ -14,7 +13,7 @@ export const authConfig = {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return NextResponse.redirect(new URL("/dashboard", nextUrl));
+        return Response.redirect(new URL("/dashboard", nextUrl));
       }
       return true;
     },
